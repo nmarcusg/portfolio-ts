f65bf7d1e7cfabc96084ecb79636ac7f207df713
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-08-22 22:37:50 +0800

    initialized navigation bar and layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationBar from './NavigationBar';
+
+const Layout: React.FC = () => {
+  return (
+    <header>
+      <NavigationBar />
+    </header>
+  );
+};
+
+export default Layout;
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,5 @@
+const NavigationBar: React.FC = () => {
+  return <nav>test</nav>;
+};
+
+export default NavigationBar;
